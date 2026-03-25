0f7c266afd933fcf2e64221bcbbf3c11dc6c6c1b
AlphaGotReal <subhampatra2411@gmail.com>
2026-03-25 22:34:22 +0530

    reward and model version controlling

--- lsy_drone_racing/control/full_metal_rl.py
+++ lsy_drone_racing/control/full_metal_rl.py
@@ -13,28 +13,35 @@ class FullMetalRL(Controller):
             info: dict | None = None) -> NDArray[np.floating]:
 
         # obs
-        # - pos              : [x, y, z]
-        # - quat             : [qx, qy, qz, qw]
-        # - vel              : [vx, vy, vz]
-        # - ang_vel          : [wx, wy, wz]
-        # - target_gate      : i (index of the next gate)
-        # - gates_pos        : [[x0, y0, z0], [x1, y1, z1] ...]
-        # - gates_quat       : [[qx0, qy0, qz0, qw0], [qx1, qy1, qz1, qw1] ...]
-        # - gates_visited    : [b0, b1, b2...] (boolean)
-        # - obstacles_pos    : [[x0, y0, z0], [x1, y1, z1] ...]
-        # - obstacles_visited: [b0, b1, b2...] (boolean)
+        # pos              : [x, y, z]
+        # quat             : [qx, qy, qz, qw]
+        # vel              : [vx, vy, vz]
+        # ang_vel          : [wx, wy, wz]
+        # target_gate      : i (index of the next gate)
+        # gates_pos        : [[x0, y0, z0], [x1, y1, z1] ...]
+        # gates_quat       : [[qx0, qy0, qz0, qw0], [qx1, qy1, qz1, qw1] ...]
+        # gates_visited    : [b0, b1, b2...] (boolean)
+        # obstacles_pos    : [[x0, y0, z0], [x1, y1, z1] ...]
+        # obstacles_visited: [b0, b1, b2...] (boolean)
 
-        action = np.array([0.0, 0.0, 0.0, 0.6], dtype=np.float32)
-        return action
+        # act
+        # act[0] = r_des
+        # act[1] = p_des
+        # act[2] = y_des
+        # act[3] = t_des
+
+        act = np.array([0.0, 0.0, 0.0, 0.6], dtype=np.float32)
+        return act
 
     def step_callback(
             self,
-            action: NDArray[np.floating],
+            act: NDArray[np.floating],
             obs: dict[str, NDArray[np.floating]],
             reward: float,
             terminated: bool,
             truncated: bool,
             info: dict) -> bool:
+        print(terminated, truncated)
         return False
 
     def episode_callback(self):
---
+++ lsy_drone_racing/rewards/v0.py
@@ -0,0 +1,20 @@
+# reward
+
+# positional
+# dx = (gates_pos[i][0] - x)
+# dy = (gates_pos[i][1] - y)
+# dz = (gates_pos[i][2] - z)
+# dr = dx^2 + dy^2 + dz^2
+
+# orientational
+# yv  = atan2(qw, qz) `yaw vector`
+# ur  = cos(yv)       `up right cosine` 
+# yaw = atan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy * qy + qz * qz))
+
+from numpy.typing import NDArray
+
+def reward(
+        obs: dict[str, NDArray(np.floating)],
+        act: NDArray(np.floating)) -> float:
+    return 0.0
+
